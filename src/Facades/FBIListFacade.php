1b58f89b87870d55310dc44abe3356f55c9f2b14
1026jota <1026jota@gmail.com>
2021-07-26 17:11:37 -0500

    remake this folder

---
+++ src/Facades/FBIListFacade.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Jota\FbiList\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class FBIListFacade extends Facade
+{
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'FBIList';
+    }
+}
